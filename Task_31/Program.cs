7658de788c2913d1cb99694860d7d1dc3035c81a
unknown <xarus82@mail.ru>
2022-12-05 21:00:27 +0300

    Задача 31

---
+++ Task_31/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных и положительных элементов массива.
+
+int[] array = new int [12];
+Random rand = new Random();
+
+int sumPositive = 0;
+int sumNegative = 0;
+
+for (int i=0; i<array.Length; i++)
+{
+    array[i] = rand.Next(-9, 10);
+}
+
+System.Console.WriteLine('[' + string.Join(", ", array) + ']');
+
+for (int i=0; i<array.Length; i++)
+{
+    if(array[i]>0)
+    {
+        sumPositive+=array[i];
+    }
+
+    else
+    {
+    sumNegative+=array[i];
+    }
+}
+
+Console.WriteLine($"Сумма положительных элементов = {sumPositive}, отрицательных = {sumNegative}");
